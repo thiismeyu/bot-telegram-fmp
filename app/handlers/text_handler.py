e55d63a8b8b6fa28d78b01b691860cfba2e1bc3a
Ayu Lutfiah <ayulutfiah21@gmail.com>
2026-04-11 03:09:19 +0800

    hapus

--- app/handlers/text_handler.py
+++ app/handlers/text_handler.py
@@ -683,58 +683,6 @@ def find_label_column(ws, row, label):
         col += 1
 
     return None
-# ================= MENU =================
-def admin_menu():
-    return ReplyKeyboardMarkup(
-        [
-            ["📊 Dashboard User"],
-            ["📊 Dashboard GAMAS"],
-            ["👥 Kelola User"],
-            ["📋 List Pending"],
-            ["🔙 KEMBALI"]
-        ],
-        resize_keyboard=True
-    )
-    
-def main_menu():
-    return ReplyKeyboardMarkup(
-        [["📝 Input Laporan","📸 Upload Foto"],
-         ["🔙 KEMBALI"]],
-        resize_keyboard=True
-    )
-
-def sto_menu():
-    return ReplyKeyboardMarkup(
-        [["BBU","WNC","UNH"],
-         ["🔙 KEMBALI"]],
-        resize_keyboard=True
-    )
-
-
-
-def preview_menu():
-    return ReplyKeyboardMarkup(
-        [["💾 SIMPAN"],
-         ["❌ BATAL"],
-         ["🔙 KEMBALI"]],
-        resize_keyboard=True
-    )
-
-def foto_menu():
-    return ReplyKeyboardMarkup(
-        [["➕ TAMBAH FOTO"],
-         ["✅ SELESAI"],
-         ["🔙 KEMBALI"]],
-        resize_keyboard=True
-    )
-def ket_menu():
-    return ReplyKeyboardMarkup(
-        [["GAMAS BESAR","GAMAS KECIL"],
-         ["INFRA CARE","LAINNYA"],
-         ["🔙 KEMBALI"]],
-        resize_keyboard=True
-    )
-
 
 # ================= PREVIEW BUILDER =================
 def build_preview(d):
